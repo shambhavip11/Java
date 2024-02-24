0070e50ec808975e249cf9a78c959819c859d19d
shambhavipanchwagh <148235646+shambhavip11@users.noreply.github.com>
2024-02-24 20:45:56 +0530

    Add files via upload

---
+++ arithmatic.java
@@ -0,0 +1,10 @@
+public class arithmatic {
+    public static void main(String[] args) {
+        
+        int a = 30;
+        int b = 10;
+
+        System.out.println(a%3+(a+10));
+        ;
+    }
+}
